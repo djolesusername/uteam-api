5b54b71749258c8b58b2b55db3c453e5faf26837
Djordje <djole001@gmail.com>
2021-12-22 11:46:14 +0100

    first commit :)

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response,NextFunction } from "express";
+ 
+
+const app: Application  = express()
+
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hello')
+})
+
+
+
+app.listen(process.env.APP_PORT, () => console.log('server running'))
